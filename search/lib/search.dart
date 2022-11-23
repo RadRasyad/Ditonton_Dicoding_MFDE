37313955601dd72d496606eaca285315dfd5031b
RadRasyad <rafidanyr21@gmail.com>
2022-11-23 16:01:51 +0700

    modularisasi 70%

---
+++ search/lib/search.dart
@@ -0,0 +1,8 @@
+library search;
+
+export 'domain/usecases/search_movies.dart';
+export 'presentation/provider/movie_search_notifier.dart';
+export 'presentation/pages/search_movie_page.dart';
+export 'domain/usecases/search_tvseries.dart';
+export 'presentation/provider/tvseries_search_notifier.dart';
+export 'presentation/pages/search_tvseries_page.dart';
