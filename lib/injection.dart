65b7f956207952a4aea92f87cff9351a1a56df5e
RadRasyad <rafidanyr21@gmail.com>
2022-12-09 14:53:20 +0700

    test false certificate work

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,6 +1,7 @@
 
 import 'package:core/data/datasources/db/database_helper.dart';
 import 'package:core/utils/network_info.dart';
+import 'package:core/utils/sllpinning.dart';
 import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:http/http.dart' as http;
 import 'package:get_it/get_it.dart';
@@ -178,6 +179,6 @@ void init() {
   locator.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(locator()));
 
   // external
-  locator.registerLazySingleton(() => http.Client());
+  locator.registerLazySingleton(() => HttpSSLPinning.client);
   locator.registerLazySingleton(() => DataConnectionChecker());
 }
